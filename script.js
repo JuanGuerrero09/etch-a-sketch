d987da4fe3e2501e5dd1c29a6a76c4d6a93604ad
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2022-10-13 19:06:17 -0500

    change grid with values added

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 // VARIABLES
-const INITIAL_GRID = 16
+const INITIAL_GRID = 9
 const INITIAL_COLOR = "#FFFFFF"
+let actual_grid = INITIAL_GRID
+
 
 
 //  SELECTORS
 
 const container = document.querySelector('.container')
-const row = document.createElement('div')
+const colorPicker = document.querySelector('#colorPicker')
+const eraserBtn = document.querySelector('.eraser')
+const elRange = document.querySelector('.rangeValue')
+const elInput = document.querySelector('#range')
 
+console.log(elInput.value)
 
 
 
 // EVENT LISTENERS
+
+elInput.addEventListener('input', changeValue)
+elInput.addEventListener('input', grid)
 
 
 
@@ -20,6 +29,9 @@ const row = document.createElement('div')
 
 
 function grid(n){
+    clearGrid()
+    n = actual_grid
+    const row = document.createElement('div')
     for (let i = 0; i < n; i++) {
         const item = document.createElement("div");
         
@@ -31,12 +43,25 @@ function grid(n){
 
         const newRow = row.cloneNode(true)
         newRow.classList.add("row", "row-" + i);
-        
         container.appendChild(newRow)
     }
+    return
 }
 
-grid(INITIAL_GRID)
+elRange.innerHTML = INITIAL_GRID + " x " + INITIAL_GRID
 
-console.log(row)
+function changeValue(){
+    elRange.innerHTML = elInput.value + " x " + elInput.value
+    actual_grid = elInput.value
+    console.log(actual_grid)
+}
+
+function clearGrid(){
+    container.innerHTML = ""
+}
+
+console.log(elInput.value)
+
+grid(5)
+
 
